139303dcca322c7367b482bd5765016e39bd806b
kjie920 <73219418+kjie920@users.noreply.github.com>
2020-10-30 20:11:46 +0800

    Update company.php

--- commands/company.php
+++ commands/company.php
@@ -1,5 +1,5 @@
 <?php
 
-$Bot->command("/\/help/", function ($Update, $Match) use ($Bot, $LANG) {
-    $Bot->sendMessage(["chat_id" => $Update["message"]["chat"]["id"], "parse_mode" => "markdown", "text" => $LANG["HELP"]]);
+$Bot->command("/\/company/", function ($Update, $Match) use ($Bot, $LANG) {
+    $Bot->sendMessage(["chat_id" => $Update["message"]["chat"]["id"], "parse_mode" => "markdown", "text" => $LANG["company"]]);
 });
