483f260d743d80786669cc0e1d370a0b722da012
kjie920 <73219418+kjie920@users.noreply.github.com>
2020-10-30 20:14:34 +0800

    Update private.php

--- commands/private.php
+++ commands/private.php
@@ -1,5 +1,5 @@
 <?php
 
-$Bot->command("/\/help/", function ($Update, $Match) use ($Bot, $LANG) {
-    $Bot->sendMessage(["chat_id" => $Update["message"]["chat"]["id"], "parse_mode" => "markdown", "text" => $LANG["HELP"]]);
+$Bot->command("/\/private/", function ($Update, $Match) use ($Bot, $LANG) {
+    $Bot->sendMessage(["chat_id" => $Update["message"]["chat"]["id"], "parse_mode" => "markdown", "text" => $LANG["private"]]);
 });
