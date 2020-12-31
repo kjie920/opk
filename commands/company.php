1693366e825b1e3943094c76676d111385fc01d7
kjie920 <73219418+kjie920@users.noreply.github.com>
2020-12-31 21:25:17 +0800

    Update company.php

--- commands/company.php
+++ commands/company.php
@@ -1,5 +1,5 @@
 <?php
 
-$Bot->command("/\/company/", function ($Update, $Match) use ($Bot, $LANG) {
+$Bot->command("/\/香港企業賬戶/", function ($Update, $Match) use ($Bot, $LANG) {
     $Bot->sendMessage(["chat_id" => $Update["message"]["chat"]["id"], "parse_mode" => "markdown", "text" => $LANG["company"]]);
 });
