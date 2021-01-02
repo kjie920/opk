ecdd1319ccc05096a1f5df3b7792ce0b7b1e919e
kjie920 <73219418+kjie920@users.noreply.github.com>
2021-01-02 15:46:22 +0800

    Update private.php

--- commands/private.php
+++ commands/private.php
@@ -1,5 +1,5 @@
 <?php
 
-$Bot->command("/\/香港個人賬戶/", function ($Update, $Match) use ($Bot, $LANG) {
+$Bot->command("/\/private/", function ($Update, $Match) use ($Bot, $LANG) {
     $Bot->sendMessage(["chat_id" => $Update["message"]["chat"]["id"], "parse_mode" => "markdown", "text" => $LANG["private"]]);
 });
