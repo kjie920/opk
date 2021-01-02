5e5a5e7640e1b056529fa01879b78f0137dcd0a9
kjie920 <73219418+kjie920@users.noreply.github.com>
2021-01-02 16:19:10 +0800

    Update start.php

--- commands/start.php
+++ commands/start.php
@@ -1,5 +1,5 @@
 <?php
 
-$Bot->command("/\/START/", function ($Update, $Match) use ($Bot, $LANG) {
+$Bot->command("/\/start/", function ($Update, $Match) use ($Bot, $LANG) {
     $Bot->sendMessage(["chat_id" => $Update["message"]["chat"]["id"], "parse_mode" => "markdown", "text" => $LANG["START"]]);
 });
