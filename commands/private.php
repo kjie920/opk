0c881ac3c931134781a8f3db590ce64b1538d3ec
kjie920 <73219418+kjie920@users.noreply.github.com>
2021-01-11 16:39:33 +0800

    Update private.php

--- commands/private.php
+++ commands/private.php
@@ -1,5 +1,5 @@
 <?php
 
 $Bot->command("/\/private/", function ($Update, $Match) use ($Bot, $LANG) {
-    $Bot->sendMessage(["chat_id" => $Update["message"]["chat"]["id"], "parse_mode" => "markdown", "text" => $LANG["private"]]);
+    $Bot->sendMessage(["chat_id" => $Update["message"]["chat"]["id"], "parse_mode" => "markdown", "text" => $LANG["PRIVATE"]]);
 });
